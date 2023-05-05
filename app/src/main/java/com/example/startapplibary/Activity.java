9c999355e3833e839ae02ffc3013dcc929d4dcac
yuvalitach <yuvalitach1@gmail.com>
2023-05-05 17:41:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/startapplibary/Activity.java
@@ -0,0 +1,14 @@
+package com.example.startapplibary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_);
+    }
+}
---
+++ app/src/main/java/com/example/startapplibary/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.startapplibary;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MotionLayout motionLayout = findViewById(R.id.motionLayout);
+        motionLayout.setTransitionListener(new MotionLayout.TransitionListener() {
+            @Override
+            public void onTransitionStarted(MotionLayout motionLayout, int startId, int endId) {
+
+            }
+
+            @Override
+            public void onTransitionChange(MotionLayout motionLayout, int startId, int endId, float progress) {
+
+            }
+
+            @Override
+            public void onTransitionCompleted(MotionLayout motionLayout, int currentId) {
+                startActivity(new Intent(MainActivity.this, Activity.class));
+            }
+
+            @Override
+            public void onTransitionTrigger(MotionLayout motionLayout, int triggerId, boolean positive, float progress) {
+
+            }
+        });
+    }
+}
